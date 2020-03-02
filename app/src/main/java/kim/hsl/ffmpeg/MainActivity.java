7763e546c920ce974e9a260465d51715afd299c5
octopus <octopus_truth@163.com>
2020-03-02 21:40:22 +0800

    update

--- app/src/main/java/kim/hsl/ffmpeg/MainActivity.java
+++ app/src/main/java/kim/hsl/ffmpeg/MainActivity.java
@@ -38,8 +38,30 @@ public class MainActivity extends AppCompatActivity {
         //设置视频显示组件
         player.setSurfaceView(surfaceView);
 
+        /*
+        RTSP协议直播源
+
+        大熊兔（点播）：rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov
+
+        RTMP协议直播源
+
+        大熊兔（点播）：rtmp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov
+
+        湖南卫视：rtmp://58.200.131.2:1935/livetv/hunantv
+
+         广西卫视 ：rtmp://58.200.131.2:1935/livetv/gxtv
+
+
+        广东卫视：rtmp://58.200.131.2:1935/livetv/gdtv
+
+
+        东方卫视：rtmp://58.200.131.2:1935/livetv/dftv 
+        */
+
         //这个是香港卫视直播地址
-        player.setDataSource("rtmp://live.hkstv.hk.lxdns.com/live/hks");
+        //player.setDataSource("rtmp://live.hkstv.hk.lxdns.com/live/hks");
+        player.setDataSource("rtmp://58.200.131.2:1935/livetv/hunantv");
+        //player.setDataSource("rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov");
 
         player.setOnPrepareListener(new Player.OnPrepareListener() {
             @Override
